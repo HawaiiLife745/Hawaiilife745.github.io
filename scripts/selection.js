f61f7c9a3e48a6a91407f6762ed46c759a73c486
HawaiiLife745 <44042916+HawaiiLife745@users.noreply.github.com>
2019-08-06 19:10:16 -0400

    Create selection.js

---
+++ scripts/selection.js
@@ -0,0 +1,103 @@
+
+// load selected cases
+loadSelection();
+renderSelection();
+
+function updateTitle() {
+    document.getElementById("csi").innerHTML = window.selCases.length;
+}
+function itemClicked(i) {
+    if (window.megaPllMap[i] == null) {
+        console.error("is null");
+        return;
+    }
+
+    var index = window.selCases.indexOf(i);
+    var wasSelected = (index != -1);
+    if (wasSelected)
+        window.selCases.splice(index, 1);
+    else
+        window.selCases.push(i);
+
+    document.getElementById("itemTd" + i).className = wasSelected ? "itemUnsel" : "itemSel" ;
+    saveSelection();
+    updateTitle();
+    renderSelection()
+}
+
+function selectAllNone() {
+    var nothingSelected = (window.selCases.length == 0);
+    if (nothingSelected) {
+        for (var i = 1; i <= 60; ++i)
+            window.selCases.push(i);
+    } else {
+        window.selCases = [];
+    }
+    renderSelection();
+    saveSelection();
+}
+
+/// \returns true if at least one case selected in group groupName
+function isAtLeastOneSelected(groupName) {
+    var indeces = algsGroups[groupName];
+    for (var i in indeces) {
+        if (selCases.indexOf(indeces[i]) != -1)
+            return true;
+    }
+    return false;
+}
+
+// select or deselect all cases in the group
+function selectMegaPllGroup(name) {
+    var aos = isAtLeastOneSelected(name);
+    var indeces = algsGroups[name];
+    for (i in indeces) {
+        if (aos) { // need to delete
+            var j = window.selCases.indexOf(indeces[i]);
+            if (j != -1)
+                window.selCases.splice(j, 1);
+        } else { // need to add
+            window.selCases.push(indeces[i]);
+        }
+    }
+    renderSelection();
+    saveSelection();
+}
+
+function makeTrNormal(groupname) {
+    var s = "";
+    var indeces = algsGroups[groupname];
+    s += "<tr><td class='megaPllgrouptdUnsel' onclick='selectMegaPllGroup(\""+groupname
+        +"\")' colspan=8><b>" + groupname + "</b></td></tr>";
+    s += "<tr>";
+    for (var j = 0; j < indeces.length; j++) {
+        var i = indeces[j]; // case number
+        var sel = (window.selCases.indexOf(i) != -1);
+        if (j===8 || j===16 || j===24 || j===32) {
+            s+= "</tr>";
+        }
+        s += "<td id='itemTd"+i+"' onclick='itemClicked("+i+")' class='"+(sel?"itemSel":"itemUnsel")+"' title='"+algsInfo[i]["name"]+"'>"+
+        "<img width='100px' id='sel"+i+"' src='pic/"+algsInfo[i]["name"]+".png' > <br>"+ algsInfo[i]["name"]+"</td>";
+    }
+    s += "</tr>";
+    return s;
+}
+
+/// iterates the megaPllMap and highlights HTML elements according to the selection
+function renderSelection()
+{
+  var s="";
+
+    s += "<table class='megaPlllayout'>";
+    //s += "<tr><a onclick='selectAllNone()'>select all or none</a>";
+    s += "<tr><td id='selectAllCases' class='megaPllgrouptdUnsel' onclick=selectAllNone(); colspan='8'><b>All Cases (60)</b> | selected: <span id='csi'></span></td></tr>";
+
+    s += makeTrNormal("CPLL");
+    s += makeTrNormal("EPLL");
+    s += makeTrNormal("1x3 Line");
+    s += makeTrNormal("2x2 Block");
+
+    s += "</table>";
+    document.getElementById("cases_selection").innerHTML = s;
+    updateTitle();
+}
